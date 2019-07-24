116a94b6d263c312199fa1f43540d72644cc8870
Ehron Gwinn <ehron@local>
2019-07-24 15:12:22 -0400

    azure deploy

---
+++ azure/mongos.tf
@@ -0,0 +1,281 @@
+
+resource "azurerm_network_interface" "mongodb-east-nic" {
+  count                     = "${var.mongos_count}"
+  name                      = "mongodb-east-NIC-${count.index}"
+  location                  = "${azurerm_resource_group.east-rg.location}"
+  resource_group_name       = "${azurerm_resource_group.east-rg.name}"
+  network_security_group_id = "${azurerm_network_security_group.east-sg.id}"
+
+  ip_configuration {
+    name                          = "mongodb-east-NicConfiguration-${count.index}"
+    subnet_id                     = "${azurerm_subnet.east-subnet.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${element(azurerm_public_ip.mongodb-east-publicip.*.id, count.index)}"
+  }
+
+  tags = {
+    ehron-autojoin  = "${var.east_dc}"
+    owner           = "ehron"
+  }
+}
+
+resource "azurerm_public_ip" "mongodb-east-publicip" {
+  count               = "${var.mongos_count}"
+  name                = "mongodb-east-publicip-${count.index}"
+  location            = "${azurerm_resource_group.east-rg.location}"
+  resource_group_name = "${azurerm_resource_group.east-rg.name}"
+  allocation_method   = "Static"
+
+  tags = {
+    owner = "ehron"
+  }
+}
+
+resource "azurerm_virtual_machine" "mongodb-east" {
+  count                 = "${var.mongos_count}"
+  name                  = "mongodb-east-${count.index}"
+  location              = "${azurerm_resource_group.east-rg.location}"
+  resource_group_name   = "${azurerm_resource_group.east-rg.name}"
+  network_interface_ids = ["${element(azurerm_network_interface.mongodb-east-nic.*.id, count.index)}"]
+  vm_size               = "${var.client_machine_type}"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "mongodb-east-disk-${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "mongodb-east-${count.index}"
+    admin_username = "${var.ssh_user}"
+    admin_password = "${var.host_pw}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+    ssh_keys {
+      key_data = "${var.ssh_public_key}"
+      path     = "/home/${var.ssh_user}/.ssh/authorized_keys"
+    }
+  }
+
+  connection {
+    user     = "${var.ssh_user}"
+    password = "${var.host_pw}"
+    #private_key = "${file(var.ssh_private_key_path)}"
+    agent = false
+    type  = "ssh"
+    host  = "${element(azurerm_public_ip.mongodb-east-publicip.*.ip_address, count.index)}"
+  }
+
+   provisioner "file" {
+    content     = "${data.template_file.azure-client-east.rendered}"
+    destination = "/tmp/client.hcl"
+  }
+
+  provisioner "file" {
+    source      = "../files/services/mongodb.hcl"
+    destination = "/tmp/mongodb.hcl"
+  }
+
+  provisioner "file" {
+    source      = "../files/use_dnsmasq.sh"
+    destination = "/tmp/use_dnsmasq.sh"
+  }
+
+  provisioner "file" {
+    source      = "../files/dnsmasq.conf"
+    destination = "/tmp/dnsmasq.conf"
+  }
+
+  provisioner "file" {
+    source      = "${var.aws_credentials_path}"
+    destination = "/tmp/credentials"
+  }
+
+  provisioner "file" {
+    source      = "../files/install_mongodb.sh"
+    destination = "/tmp/install_mongodb.sh"
+  }
+
+  provisioner "file" {
+    source      = "${var.aws_credentials_path}"
+    destination = "/tmp/credentials"
+  }
+
+  provisioner "file" {
+    source      = "../files/install_envoy.sh"
+    destination = "/tmp/install_envoy.sh"
+  }
+
+  provisioner "file" {
+    source      = "../files/install_consul_proxy.sh"
+    destination = "/tmp/install_consul_proxy.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "${var.install_consul}",
+      "${var.set_consul_client_conf}",
+      "${var.set_consul_mongo_conf}",
+      "${var.install_envconsul}",
+      "${var.use_dnsmasq}",
+      "${var.install_mongodb_and_proxy}"
+      #"${var.sync_envoy}"
+    ]
+  }
+}
+
+
+resource "azurerm_network_interface" "mongodb-west-nic" {
+  count                     = "${var.mongos_count}"
+  name                      = "mongodb-west-NIC-${count.index}"
+  location                  = "${azurerm_resource_group.west-rg.location}"
+  resource_group_name       = "${azurerm_resource_group.west-rg.name}"
+  network_security_group_id = "${azurerm_network_security_group.west-sg.id}"
+
+  ip_configuration {
+    name                          = "mongodb-west-NicConfiguration-${count.index}"
+    subnet_id                     = "${azurerm_subnet.west-subnet.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${element(azurerm_public_ip.mongodb-west-publicip.*.id, count.index)}"
+  }
+
+  tags = {
+    ehron-autojoin  = "${var.west_dc}"
+    owner           = "ehron"
+  }
+}
+
+resource "azurerm_public_ip" "mongodb-west-publicip" {
+  count               = "${var.mongos_count}"
+  name                = "mongodb-west-publicip-${count.index}"
+  location            = "${azurerm_resource_group.west-rg.location}"
+  resource_group_name = "${azurerm_resource_group.west-rg.name}"
+  allocation_method   = "Static"
+
+  tags = {
+    owner = "ehron"
+  }
+}
+
+resource "azurerm_virtual_machine" "mongodb-west" {
+  count                 = "${var.mongos_count}"
+  name                  = "mongodb-west-${count.index}"
+  location              = "${azurerm_resource_group.west-rg.location}"
+  resource_group_name   = "${azurerm_resource_group.west-rg.name}"
+  network_interface_ids = ["${element(azurerm_network_interface.mongodb-west-nic.*.id, count.index)}"]
+  vm_size               = "${var.client_machine_type}"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "mongodb-west-disk-${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "mongodb-west-${count.index}"
+    admin_username = "${var.ssh_user}"
+    admin_password = "${var.host_pw}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+    ssh_keys {
+      key_data = "${var.ssh_public_key}"
+      path     = "/home/${var.ssh_user}/.ssh/authorized_keys"
+    }
+  }
+
+  connection {
+    user     = "${var.ssh_user}"
+    password = "${var.host_pw}"
+    #private_key = "${file(var.ssh_private_key_path)}"
+    agent = false
+    type  = "ssh"
+    host  = "${element(azurerm_public_ip.mongodb-west-publicip.*.ip_address, count.index)}"
+  }
+
+   provisioner "file" {
+    content     = "${data.template_file.azure-client-west.rendered}"
+    destination = "/tmp/client.hcl"
+  }
+
+  provisioner "file" {
+    source      = "../files/services/mongodb.hcl"
+    destination = "/tmp/mongodb.hcl"
+  }
+
+  provisioner "file" {
+    source      = "../files/use_dnsmasq.sh"
+    destination = "/tmp/use_dnsmasq.sh"
+  }
+
+  provisioner "file" {
+    source      = "../files/dnsmasq.conf"
+    destination = "/tmp/dnsmasq.conf"
+  }
+
+  provisioner "file" {
+    source      = "${var.aws_credentials_path}"
+    destination = "/tmp/credentials"
+  }
+
+  provisioner "file" {
+    source      = "../files/install_mongodb.sh"
+    destination = "/tmp/install_mongodb.sh"
+  }
+
+  provisioner "file" {
+    source      = "${var.aws_credentials_path}"
+    destination = "/tmp/credentials"
+  }
+
+  provisioner "file" {
+    source      = "../files/install_envoy.sh"
+    destination = "/tmp/install_envoy.sh"
+  }
+
+  provisioner "file" {
+    source      = "../files/install_consul_proxy.sh"
+    destination = "/tmp/install_consul_proxy.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "${var.install_consul}",
+      "${var.set_consul_client_conf}",
+      "${var.set_consul_mongo_conf}",
+      "${var.install_envconsul}",
+      "${var.use_dnsmasq}",
+      "${var.install_mongodb_and_proxy}"
+      #"${var.sync_envoy}"
+    ]
+  }
+}
